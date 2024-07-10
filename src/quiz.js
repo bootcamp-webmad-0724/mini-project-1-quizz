39b1b3e40605549efff1f2e45fc413d72d95bd3a
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2024-07-10 14:36:10 +0200

    Update quiz.js with array methods

--- src/quiz.js
+++ src/quiz.js
@@ -30,4 +30,17 @@ class Quiz {
     hasEnded() {
         return this.currentQuestionIndex >= this.questions.length
     }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (isNaN(difficulty) || !(difficulty > 0 && difficulty < 4) || typeof difficulty != 'number') {
+            return
+        }
+        const filteredQuestions = this.questions.filter(eachQuestion => eachQuestion.difficulty === difficulty)
+        this.questions = filteredQuestions
+    }
+
+    averageDifficulty() {
+        const sumDifficulty = this.questions.reduce((acc, eachQuestion) => acc + eachQuestion.difficulty, 0)
+        return sumDifficulty / this.questions.length
+    }
 }
